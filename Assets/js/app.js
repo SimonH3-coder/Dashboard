ca56065714f806f7de398890b83ac6f8722f82bb
Simon Hasselflug <simo16f4@elev.techcollege.dk>
2025-11-12 13:31:07 +0100

    Revert "Feature/news feed"

--- Assets/js/app.js
+++
@@ -1,8 +0,0 @@
-// import './controller/canteenController.js'
-// import './model/canteenModel.js'
-// import "./controller/weatherController.js";
-// import { startClock } from "./renderClock.js";
-import { startNewsFeed } from "./controller/newsController.js";
-
-// startClock();
-startNewsFeed();
