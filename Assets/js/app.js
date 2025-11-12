fd94d873adcfd557b87315f23aac21663e1ba52d
Seba010d <Seba010d@gmail.com>
2025-11-12 13:11:53 +0100

    #7 i think i made it

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,3 +1,8 @@
 // import './controller/canteenController.js'
 // import './model/canteenModel.js'
-import './controller/weatherController.js'
+// import "./controller/weatherController.js";
+// import { startClock } from "./renderClock.js";
+import { startNewsFeed } from "./controller/newsController.js";
+
+// startClock();
+startNewsFeed();
--- Assets/js/model/fetchAPIController.js
+++ Assets/js/model/fetchAPIController.js
@@ -1,4 +1,4 @@
-async function fetchAPI(url) {
+export async function fetchAPI(url) {
   try {
     const res = await fetch(url)
     if (!res.ok) throw new Error(`HTTP ${res.status}: ${res.statusText}`)
